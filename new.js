eaf52f40e2530fa06e66f11f0d524567face1508
Joshua Bolanos <josh.bolanos7@gmail.com>
2022-01-26 20:57:07 -0500

    added results for player choices and computer choices.

--- new.js
+++ new.js
@@ -8,3 +8,42 @@ const choices = ['rock', 'paper', 'scissors']
 let playerChoice 
 let computerChoice 
 
+const handleClick = (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innterHTML = 'Player choice: ' + playerChoice
+    generateComputerChoice()
+    getResult()
+}
+
+const generateComputerChoice = () => { 
+    const randomChoice = choices[Math.random(Math.random() * choices.length)]
+    computerChoice = randomChoice 
+    computerChoiceDisplay.innerHTML = 'Computer choice: ' + computerChoice
+}
+
+for (let i = 0; i < choices.length; i ++) { 
+    const button = documet.createElement('button')
+    button.id = choices[i]
+    button.addEventListener('click', handleClick)
+    gameGrid.appendChild(button)
+}
+
+const getResult = () => { 
+    switch (playerChoice + computerChoice) { 
+        case 'scissorspaper':
+        case 'rocksscirssors':
+        case 'paperrock':
+            resultDisplay.innerHTML = "YOU WIN!"
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = "YOU LOSE!"
+            break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+        resultDisplay.innerHTML = "ITS A DRAW"
+        break
+    }
+}
